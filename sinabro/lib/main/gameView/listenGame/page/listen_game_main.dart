1d13de89ea55b59c88829d1912d943283c189458
SJHawa <163496255+SJHawa@users.noreply.github.com>
2025-11-04 05:52:28 +0900

    .

--- sinabro/lib/main/gameView/listenGame/page/listen_game_main.dart
+++ sinabro/lib/main/gameView/listenGame/page/listen_game_main.dart
@@ -1,6 +1,8 @@
 // lib/main/gameView/listenGame/page/listen_game_main.dart
 import 'package:flutter/material.dart';
-import 'package:sinabro/main/gameView/listenGame/page/listen_game_flow.dart';
+import 'package:sinabro/main/gameView/listenGame/page/level1/level1_flow.dart';
+import 'package:sinabro/main/gameView/listenGame/page/level2/level2_flow.dart';
+import 'package:sinabro/main/gameView/listenGame/page/level3/level3_flow.dart';
 
 /// 듣기 게임 진입 페이지
 /// - level 값에 따라 Flow 실행
